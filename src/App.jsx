19841d7e44fd9c7531d9ef601bf6e8b258139532
Learnerbypassion <callmedad002022@gmail.com>
2026-02-10 15:02:14 +0530

    Fixing the issue if casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
 import Landingpage from './pages/LandingPage.jsx';
-import CreatepostPage from './pages/createpostPage.jsx';
+import CreatepostPage from './pages/CreatepostPage.jsx';
 import ShowPost from './pages/showPost.jsx';
 const App = () => {
   return (
